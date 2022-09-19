323091dd32a6740e4134d1120adc6264aaa8e19f
pobiq <111417407+pobiq@users.noreply.github.com>
2022-09-19 19:02:02 +0900

    Time: 1 ms (98.96%), Space: 42.7 MB (94.58%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] result = new int[nums.length*2];
+        
+        for(int i=0; i<nums.length; i++) {
+            result[i] = nums[i];
+        }
+        
+        for(int i=0; i<nums.length; i++) {
+            result[nums.length + i] = nums[i];
+        }
+        
+        return result;
+    }
+}
